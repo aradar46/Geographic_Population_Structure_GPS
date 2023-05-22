b25415ab34e0c73c6619ee095a790d9bf18a49b4
Arash Darzian <arash.darzian@outlook.com>
2023-05-22 13:35:39 +0200

    Update GPSmaf_streamlit_app.py

--- Scripts_notebooks/GPSmaf_streamlit_app.py
+++ Scripts_notebooks/GPSmaf_streamlit_app.py
@@ -1,7 +1,7 @@
 # import libraries
 import streamlit as st
 import pandas as pd
-import umap
+from umap import umap
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
