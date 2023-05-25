82ef1e143793e58c07b0b80e3efaeaa357678349
arash.bmc <arash@bmclab.com>
2023-05-25 15:05:32 +0200

    add final changes related to submission including report, minor changes, alignments etc

--- Scripts_notebooks/GPSmaf_streamlit_app.py
+++ Scripts_notebooks/GPSmaf_streamlit_app.py
@@ -27,6 +27,13 @@ st.title("GPSmaf")
 # Subheader
 st.subheader("Geographical Population Structure Infrence from Minor Allele Frequencies")
 
+# Description
+st.write('How to use this app:')
+st.write('1. Select the dates you want to include in the analysis')
+st.write('2. Select the countries you want to include in the analysis. If you select all countries, the analysis will be done for all countries')
+st.write('3. Select the number of neighbors for the UMAP projection. The default will be calculated automatically, based on the number of country-date groups selected')
+st.write('4. Run the analysis and the UMAP projection and dendrogram will be shown. You can hover over the points to see the country and date of each point')
+
 ################################################################################
 # functions
 def umap_plot(df, data_label, colors_map, num_neigh):
@@ -66,35 +73,37 @@ def umap_plot(df, data_label, colors_map, num_neigh):
     for i, label in enumerate(unique_labels):
         label_data = df_plot[df_plot["colors_map"] == label]
         fig.add_trace(
-            go.Scatter(
-                x=label_data["UMAP1"],
-                y=label_data["UMAP2"],
-                mode="markers",
-                marker=dict(
-                    color=color_scale[i % len(color_scale)],
-                ),
-                text=label_data["data_label"],
-                name=str(label),
-            )
+        go.Scatter(
+            x=label_data["UMAP1"],
+            y=label_data["UMAP2"],
+            mode="markers",
+            marker=dict(
+                color=color_scale[i % len(color_scale)],
+            ),
+            text=label_data["data_label"],
+            textposition="top center",  # Show labels without hover
+            name=str(label),
         )
+    )
+    # make points bigger
     
     #make background black
     fig.update_yaxes(
         # mirror=True,
         ticks='outside',
         # showline=True,
-        linecolor='#101010',
-        gridcolor='#101010'
-                    )      
+        linecolor='white',
+        gridcolor='white'
+                    )
 
 
-    fig.update_traces(marker=dict(size=5), textfont=dict(size=8))
+    fig.update_traces(marker=dict(size=8), textfont=dict(size=8))
     fig.update_layout(
         title="UMAP projection", title_x=0.5,
         xaxis_title="UMAP1",
         yaxis_title="UMAP2",
         showlegend=True,
-        plot_bgcolor="#101010",
+        # plot_bgcolor="#101010",
 
         
     )
@@ -127,7 +136,7 @@ def make_dendrogram(df_dist):
     
     fig.update_layout(xaxis_tickangle=-90,
         font=dict(size=4),
-        hoverlabel=dict(bgcolor="#ff4b4b", font_size=16, font_family="Rockwell"),
+        hoverlabel=dict( font_size=16, font_family="Rockwell"),
         width=600, height=800,
         title_text="Dendrogram",
         title_x=0.5,
@@ -185,39 +194,73 @@ num_neigh = st.sidebar.slider("Select number of neighbors:", 2, 40, mid_neg, hel
 # chose between country_region 	country_date	country 
 # Using tooltip with a button
 tooltip1 = "Label which will be shown in the plot and dendrogram."
-group_by = st.sidebar.selectbox( "Groups Label:", ["country", "country_date"], help=tooltip1)
+group_by = 'country'
 # if user hover over the question mark, show the following text
 
-
+if len(dates)>1:
+    group_by = "country_date"
 
 
 
 ################################################################################
 # sidebar button
 if st.sidebar.button("Run"):
-    col1, col2, col3 = st.columns([10, 1, 10])
-    with col1:
-        my_encoded_data = df.iloc[:, 7:].values
-        # Plot
-        # try:
-        df_dist, fig = umap_plot(
-            my_encoded_data,
-            df[group_by],
-            df["region"].str.title(),
-            num_neigh=num_neigh,
-        )
-        st.write(fig)
-        # print number of points
+    if len(df)<5:
+        st.header("There is not enough data to run the analysis. Please select more countries and dates.")
+    else:
+        try:
+            col1, col2, col3 = st.columns([10, 1, 10])
+            with col1:
+                my_encoded_data = df.iloc[:, 7:].values
+                # Plot
+                # try:
+                df_dist, fig = umap_plot(
+                    my_encoded_data,
+                    df[group_by],
+                    df["region"].str.title(),
+                    num_neigh=num_neigh,
+                )
+                st.write(fig)
+                # print number of points
 
-    with col2:
-        st.write("")
+            with col2:
+                st.write("")
 
-    with col3:
-        # dendrogram
-        dendo = make_dendrogram(df_dist)
-        st.write(dendo)
-        # except:
-        #     st.write("Please select at least one region or country and one date")
-
+            with col3:
+                # dendrogram
+                dendo = make_dendrogram(df_dist)
+                st.write(dendo)
+                # except:
+                #     st.write("Please select at least one region or country and one date")
+        except:
+            st.header("Please select at least one country and one date. The more better.")
+ 
 
 ################################################################################
+
+
+
+# write a alert
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+st.sidebar.write("Some tips:")
+st.sidebar.write("--> If the points are scattered, try to select smaller number of neighbors")
+st.sidebar.write("--> The plots are interactive, you can zoom in and out and save the plots")
+st.sidebar.write("--> If you select more than one date, the labels will be shown as < countryName + _ + int(bp_date/1000) >")
+
+
+
+# github link
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+
+# hyperlink
+# 
+st.sidebar.markdown(
+    """<a href="https://github.com/arash-darzian/Geographic_Population_Structure_GPS" target="_blank">GitHub Repository</a>""",
+    unsafe_allow_html=True,
+)
